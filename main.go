9007e7974cc7a370c71452366d719a9aa3970f0e
lihong <lihongitchina@gmail.com>
2018-11-11 20:02:03 +0800

    update entry

--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"github.com/researchlab/crawler/engine"
+	"github.com/researchlab/crawler/scheduler"
 	"github.com/researchlab/crawler/zhenai/parser"
 )
 
 func main() {
-	engine.Run(engine.Request{
+
+	(&engine.ConcurrentEngine{
+		Scheduler:   &scheduler.QueuedScheduler{},
+		WorkerCount: 100,
+	}).Run(engine.Request{
 		Url:        "http://www.zhenai.com/zhenghun",
 		ParserFunc: parser.ParseCityList,
 	})
