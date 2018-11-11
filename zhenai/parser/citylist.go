6e3be48fd634b8809014f5356598935f1ea5ef8f
lihong <lihongitchina@gmail.com>
2018-11-11 21:04:03 +0800

    clean code

--- zhenai/parser/citylist.go
+++ zhenai/parser/citylist.go
@@ -14,8 +14,8 @@ func ParseCityList(contents []byte) engine.ParseResult {
 
 	result := engine.ParseResult{}
 	for _, m := range matches {
-		result.Items = append(
-			result.Items, string(m[2]))
+		//		result.Items = append(
+		//			result.Items, string(m[2]))
 		result.Requests = append(
 			result.Requests, engine.Request{
 				Url:        string(m[1]),
