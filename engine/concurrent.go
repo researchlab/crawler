22f1d178a4331389d43e5ce40d90456334de7def
lihong <lihongitchina@gmail.com>
2018-11-11 20:01:02 +0800

    update engine

---
+++ engine/concurrent.go
@@ -0,0 +1,67 @@
+package engine
+
+import (
+	"log"
+
+	"github.com/researchlab/crawler/fetcher"
+)
+
+type ConcurrentEngine struct {
+	Scheduler   Scheduler
+	WorkerCount int
+}
+
+type Scheduler interface {
+	Submit(Request)
+	ConfigureMasterWorkerChan(chan Request)
+	WorkerReady(chan Request)
+	Run()
+}
+
+func (p *ConcurrentEngine) Run(seeds ...Request) {
+	out := make(chan ParseResult)
+	p.Scheduler.Run()
+
+	for i := 0; i < p.WorkerCount; i++ {
+		createWorker(out, p.Scheduler)
+	}
+
+	for _, r := range seeds {
+		p.Scheduler.Submit(r)
+	}
+
+	for {
+		result := <-out
+		for _, item := range result.Items {
+			log.Printf("Got item: %v\n", item)
+		}
+		for _, request := range result.Requests {
+			p.Scheduler.Submit(request)
+		}
+	}
+}
+
+func createWorker(out chan ParseResult, s Scheduler) {
+	in := make(chan Request)
+	go func() {
+		for {
+			s.WorkerReady(in)
+			request := <-in
+			result, err := worker(request)
+			if err != nil {
+				continue
+			}
+			out <- result
+		}
+	}()
+}
+
+func worker(r Request) (ParseResult, error) {
+	log.Printf("Fetching %s", r.Url)
+	body, err := fetcher.Fetch(r.Url)
+	if err != nil {
+		log.Printf("Fetcher: error fetching url %s: %v", r.Url, err)
+		return ParseResult{}, err
+	}
+	return r.ParserFunc(body), nil
+}
--- engine/simple.go
+++ engine/simple.go
@@ -6,7 +6,9 @@ import (
 	"github.com/researchlab/crawler/fetcher"
 )
 
-func Run(seeds ...Request) {
+type SimpleEngine struct{}
+
+func (p *SimpleEngine) Run(seeds ...Request) {
 	var requests []Request
 	for _, r := range seeds {
 		requests = append(requests, r)
@@ -15,18 +17,24 @@ func Run(seeds ...Request) {
 	for len(requests) > 0 {
 		r := requests[0]
 		requests = requests[1:]
-		log.Printf("Fetching %s", r.Url)
-		body, err := fetcher.Fetch(r.Url)
+		parseResult, err := p.worker(r)
 		if err != nil {
-			log.Printf("Fetcher: error fetching url %s: %v", r.Url, err)
 			continue
 		}
-
-		parseResult := r.ParserFunc(body)
 		requests = append(requests,
 			parseResult.Requests...)
 		for _, item := range parseResult.Items {
 			log.Printf("Got item %v", item)
 		}
 	}
+}
+
+func (p *SimpleEngine) worker(r Request) (ParseResult, error) {
+	log.Printf("Fetching %s", r.Url)
+	body, err := fetcher.Fetch(r.Url)
+	if err != nil {
+		log.Printf("Fetcher: error fetching url %s: %v", r.Url, err)
+		return ParseResult{}, err
+	}
+	return r.ParserFunc(body), nil
 }
