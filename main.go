184928fc5883b2995b44c0b5bad506291f8a6539
lihong <lihongitchina@gmail.com>
2018-11-12 19:33:12 +0800

    update entry

--- main.go
+++ main.go
@@ -2,22 +2,27 @@ package main
 
 import (
 	"github.com/researchlab/crawler/engine"
+	"github.com/researchlab/crawler/persist"
 	"github.com/researchlab/crawler/scheduler"
 	"github.com/researchlab/crawler/zhenai/parser"
 )
 
 func main() {
-
+	itemChan, err := persist.ItemSaver("dating_profile")
+	if err != nil {
+		panic(err)
+	}
 	e := (&engine.ConcurrentEngine{
 		Scheduler:   &scheduler.QueuedScheduler{},
-		WorkerCount: 100,
+		WorkerCount: 1000,
+		ItemChan:    itemChan,
+	})
+	e.Run(engine.Request{
+		Url:        "http://www.zhenai.com/zhenghun",
+		ParserFunc: parser.ParseCityList,
 	})
 	//e.Run(engine.Request{
-	//	Url:        "http://www.zhenai.com/zhenghun",
-	//	ParserFunc: parser.ParseCityList,
+	//	Url:        "http://www.zhenai.com/zhenghun/shanghai",
+	//	ParserFunc: parser.ParseCity,
 	//})
-	e.Run(engine.Request{
-		Url:        "http://www.zhenai.com/zhenghun/shanghai",
-		ParserFunc: parser.ParseCity,
-	})
 }
